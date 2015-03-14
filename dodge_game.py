20561313d5fbc512ecd3f6945189e57e3fe78852
kramretsel <marklester.belbis@bicol-u.edu.ph>
2015-03-14 15:59:23 +0800

    Update dodge_game.py

--- dodge_game.py
+++ dodge_game.py
@@ -12,6 +12,7 @@ def quit_game():
 def main():
 
     pygame.init()
+    pygame.font.init()
 
     width = 800
     height = 600
@@ -31,9 +32,11 @@ def main():
     
     score = 0
     startpos = [375, 480]
+    player_width = 60
     timer = 100
     timer1 = 0
     enemies = [[800, 600]]
+    distance = 50
     clock = pygame.time.Clock()
     
     frame_count = 0
@@ -41,7 +44,7 @@ def main():
     start_time = 90
 
     def score_count(count):
-        font = pygame.font.Font("comicsansms", 30)
+        font = pygame.font.SysFont("comicsansms", 30)
         text = font.render("Score : "+str(count), True, white)
         screen.blit(text, (60,0))
 
@@ -50,6 +53,18 @@ def main():
     while not gameExit:
         pygame.font.init()
         timer -= 1
+        
+        #timer
+        total_seconds = start_time - (frame_count // frame_rate)
+        if total_seconds < 0:
+            total_seconds = 0
+
+        minutes = total_seconds // 60
+        seconds = total_seconds % 60
+        output_string = "Time: {0:02}:{1:02}".format(minutes, seconds)
+        
+        font = pygame.font.SysFont("comicsansms", 30)
+        text = font.render(output_string, True, white)
         
         #add enemies
         if timer == 0:
@@ -113,22 +128,47 @@ def main():
                 if 650 > mouse[0] > 550 and 500 > mouse[1] > 450 and click[0] == 1:
                     quit_game()
                     
-                pygame.display.update()            
-
-                        
+                pygame.display.update()      
             
-        #timer
-        total_seconds = start_time - (frame_count // frame_rate)
-        if total_seconds < 0:
-            total_seconds = 0
+            #collision with wall
+            if startpos[0] > width - player_width or startpos[0] < 0:
+            font = pygame.font.SysFont("comicsansms", 115)
+            text = font.render("You crashed!", True, red)
+            screen.blit(text, (60, 200))
+                
+            while True:           
+                for event in pygame.event.get():
+                       if event.type == pygame.QUIT:
+                           pygame.quit()
+                           quit()
+                           
+                mouse1 = pygame.mouse.get_pos()
+                click1 = pygame.mouse.get_pressed()
+                
+                pygame.draw.rect(screen, green, (150, 450, 100, 50))
+                smallText1 = pygame.font.SysFont("comicsansms", 20)
+                textSurf1, textRect1 = text_objects("Retry", smallText1)
+                textRect1.center = ((150 + (100/2)), (450 + (50/2)))
+                screen.blit(textSurf1, textRect1)
 
-        minutes = total_seconds // 60
-        seconds = total_seconds % 60
-        output_string = "Time: {0:02}:{1:02}".format(minutes, seconds)
-        
-        font = pygame.font.Font("comicsansms", 30)
-        text = font.render(output_string, True, white)
-        
+                if 250 > mouse1[0] > 150 and 500 > mouse1[1] > 450 and click1[0] == 1:
+                    main()
+
+                mouse = pygame.mouse.get_pos()
+                click = pygame.mouse.get_pressed()
+
+                pygame.draw.rect(screen, red, (550, 450, 100, 50))
+                smallText = pygame.font.SysFont("comicsansms", 20)
+                textSurf, textRect = text_objects("Quit", smallText)
+                textRect.center = ((550 + (100/2)), (450 + (50/2)))
+                screen.blit(textSurf, textRect)
+
+                if 650 > mouse[0] > 550 and 500 > mouse[1] > 450 and click[0] == 1:
+                    quit_game()
+                    
+                pygame.display.update()
+
+            
         #update screen      
         screen.fill(0)
         screen.blit(bg, (0,0))
@@ -139,9 +179,7 @@ def main():
             
         screen.blit(playerImg, startpos)
         score_count(score)
-
         
-
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -164,3 +202,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    pygame.quit()
+    quit()
